c972926f58ceb91d067f13652541299539dee3d0
Benjamin Geets <bgeets@icloud.com>
2021-03-26 22:33:44 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p>home</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
